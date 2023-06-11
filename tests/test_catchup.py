e790ccb215ed0b552db78e199e5b386b664d263b
Anshuman Dash <anshuman@ucsb.edu>
2023-06-11 00:43:46 -0700

    similar blocks

--- tests/test_catchup.py
+++ tests/test_catchup.py
@@ -26,7 +26,7 @@ class TestCatchup:
 		assert mp[3].ballot_num[0] == 0
 
 		mp[3].prepare()
-		time.sleep(.1)
+		time.sleep(.3)
 		assert mp[3].ballot_num[0] == 1
 
 	def test_catchup_proxy(self):
@@ -68,6 +68,21 @@ class TestCatchup:
 		mp[3].prepare()
 		time.sleep(1)
 		assert mp[3].ballot_num[0] == 3
+
+	def test_victory_royale(self):
+		mp = [MultiPaxos(Network(i), i, Blog(),debug_print=True) for i in range(5)]
+		mp[0].addQueue(("POST", "user", "title 1", "cont"))
+		mp[0].addQueue(("POST", "user", "title 2", "cont"))
+		mp[0].addQueue(("POST", "user", "title 3", "cont"))
+		while mp[0].ballot_num[0] < 3:
+			time.sleep(.05)
+		for i in range(1,5):
+			mp[i] = MultiPaxos(Network(i), i, Blog(),debug_print=True)
+		mp[0].addQueue(("COMMENT", "comment", "title 2", "cmnt"))
+		time.sleep(1)
+		for i in range(1,4):
+			for a,b in zip(mp[0].blog.blocks, mp[i].blog.blocks):
+				assert str(a.T) == str(b.T), i
 
 if __name__ == '__main__':
 	try:
